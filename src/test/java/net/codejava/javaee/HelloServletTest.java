2f967cb22a88faf564dca5fd8b4740457a21ff39
Badari <badari.deshpande@gmail.com>
2018-04-05 18:36:22 +0530

    Signed-off-by: Badari <badari.deshpande@gmail.com>

--- src/test/java/net/codejava/javaee/HelloServletTest.java
+++ src/test/java/net/codejava/javaee/HelloServletTest.java
@@ -31,7 +31,7 @@ public class HelloServletTest {
         assertTitleEquals("Hello World");
 		assertFormPresent();
 		assertFormElementPresent("yourName");
-		assertTextFieldEquals("name", "Enter your name here");
+		assertTextFieldEquals("yourName", "Enter your name here");
         //assertLinkPresent("home");
         //clickLink("home");
         //assertTitleEquals("Home");
